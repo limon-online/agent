c6835401edf315eca1d154e1ebf56acd04d814be
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2021-04-01 11:16:35 +0300

    Added function for get system information

--- src/linux/system.rs
+++ src/linux/system.rs
@@ -1,11 +1,47 @@
+use std::fs;
+
+use sentry;
+
 use crate::system::{System, SystemInfo};
+
+enum InfoType {
+  Name,
+  Version
+}
+
+
+fn get_system_info(info: InfoType) -> String {
+  let info_str = match info {
+    InfoType::Name => "NAME=",
+    InfoType::Version => "VERSION_ID=",
+  };
+
+  let contents = fs::read_to_string("/etc/os-release");
+
+  let contents = match contents {
+    Ok(contents) => contents,
+    Err(err) => {
+      sentry::capture_error(&err);
+      return String::from("Unknown");
+    }
+  };
+
+  for line in contents.split("\n") {
+    if line.starts_with(info_str) {
+      return line.replace(info_str, "").replace("\"", "");
+    }
+  }
+
+  String::from("Unknown")
+}
+
 
 impl SystemInfo for System {
   fn get_name() -> String {
-    String::from("Ubuntu")
+    get_system_info(InfoType::Name)
   }
 
   fn get_version() -> String {
-    String::from("20.02")
+    get_system_info(InfoType::Version)
   }
 }
--- src/system.rs
+++ src/system.rs
@@ -7,9 +7,9 @@ pub struct System {
 
 
 impl fmt::Display for System {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.name, self.version)
-    }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} {}", self.name, self.version)
+  }
 }
 
 
